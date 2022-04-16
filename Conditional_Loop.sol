2c60d5c026479bedf2cb30b5ee4647b17dd67cf0
Sol_enthusiast <101778567+SOLenthusiast@users.noreply.github.com>
2022-04-16 04:03:16 -0400

    Update Conditional_Loop.sol

--- Conditional_Loop.sol
+++ Conditional_Loop.sol
@@ -3,17 +3,26 @@ pragma solidity ^0.8.0;
 
 contract MyContract5 {
     
+    //Create an array  
+    //a function that checks if numbers are even
+    //a function that counts the even numbers in the array
+    //check if address currently interacting with smart contract is the one that deployed
+
     uint[] public numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     
+    address public owner;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
     function countEvenNumbers() public view returns (uint) {
         uint count = 0;
-
         for(uint i=0; i < numbers.length; i++) {
             if(isEvenNumber(numbers[i])) {
                 count ++;
             }
         }
-
         return count;
     }
 
@@ -24,5 +33,9 @@ contract MyContract5 {
         } else {
             return false;
         }
+    }
+
+    function IsOwner() public view returns(bool) {
+        return(msg.sender == owner);
     }
 }
