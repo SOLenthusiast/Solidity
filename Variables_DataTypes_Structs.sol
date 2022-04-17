3f3cd3e2c525f91ba7c1bd187358af93af76cdbe
Sol_enthusiast <101778567+SOLenthusiast@users.noreply.github.com>
2022-04-17 05:18:05 -0400

    Update Variables_DataTypes_Structs.sol

--- Variables_DataTypes_Structs.sol
+++ Variables_DataTypes_Structs.sol
@@ -2,29 +2,23 @@ pragma solidity ^0.8.0;
 // SPDX-License-Identifier: GPL-3.0
 contract MyContract {
 
-    //state var
-    uint public myUint = 1;
-    uint256 public myUint256 = 1;
-    uint8 public myUint8 = 1;
-    int public myInt = 1;
-    
-    //strings
-    string public myString = "Hello world";
-    bytes32 public myBytes32 = "Hello world!";
    
-    address public myAdress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    uint public MyUint = 1;
+    string public MyString = "Hello World!";
+    bytes32 public MyByte32 = "Hello world";
+    address public MyAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    int public myInt = 1;
 
-    struct MyStruct {
-       uint myInt ;
-       string myString;
+    struct John {
+        uint MyAge;
+        string Lastname;
+        string Favfood;
     }
 
-    MyStruct public myStruct = MyStruct(1, "Hello world!");
+    John public Johnie = John(21, "Doe", "Perogie");
 
-    function getValue() public pure returns(uint) {
-        
+    function getValue() public pure returns(uint) {      
         uint value = 1;     //local var
-
         return value; 
     }
 }
