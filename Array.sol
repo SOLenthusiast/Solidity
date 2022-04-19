b68b47bb3692d4b76323a1ac7fafa994cd5f3117
Sol_enthusiast <101778567+SOLenthusiast@users.noreply.github.com>
2022-04-19 06:45:05 -0400

    Update Array.sol

--- Array.sol
+++ Array.sol
@@ -1,6 +1,13 @@
 pragma solidity ^0.8.0;
 // SPDX-License-Identifier: GPL-3.0
 
+// Build 
+// 1D, 2D arrays
+// An array list of fruits
+// An empty array of string
+// A function that fills in the empty array
+// A function returns its length
+
 contract MyContracts {
     //Array
     uint[] public uintArray = [1,2,3];
