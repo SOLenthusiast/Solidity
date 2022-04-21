8cda19d98c3c44020969b25966e70a8ce12a9add
Sol_enthusiast <101778567+SOLenthusiast@users.noreply.github.com>
2022-04-21 03:33:51 -0400

    Update Conditional_Loop.sol

--- Conditional_Loop.sol
+++ Conditional_Loop.sol
@@ -1,41 +1,31 @@
 pragma solidity ^0.8.0;
 // SPDX-License-Identifier: GPL-3.0
 
-contract MyContract5 {
-    
-    //Create an array  
-    //a function that checks if numbers are even
-    //a function that counts the even numbers in the array
-    //check if address currently interacting with smart contract is the one that deployed
 
-    uint[] public numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+contract MyContract {
     
-    address public owner;
+    uint[] public tab = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    address public Owner = msg.sender;
 
-    constructor() public {
-        owner = msg.sender;
+    function isEvenNumber(uint _number) public view returns(bool) {
+        return(_number % 2 == 0);
     }
 
-    function countEvenNumbers() public view returns (uint) {
+    function isEvenNumbertab(uint index) public view returns(bool) {
+        return(tab[index] % 2 == 0);
+    }
+
+    function CountEvenOnes() public view returns(uint) {
         uint count = 0;
-        for(uint i=0; i < numbers.length; i++) {
-            if(isEvenNumber(numbers[i])) {
+        for (uint i=0; i<tab.length; i++) {
+            if(isEvenNumber(tab[i])) {
                 count ++;
             }
         }
         return count;
     }
 
-    //Conditionals
-    function isEvenNumber(uint _number) public view returns(bool){
-        if (_number % 2 == 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    function IsOwner() public view returns(bool) {
-        return(msg.sender == owner);
+    function isCurrentAddress() public view returns(bool) {
+        return(msg.sender == Owner);
     }
 }
